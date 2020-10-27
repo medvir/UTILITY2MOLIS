22441f52c587ef39e03c5cb63536c69b32d6d4e8
sschmutz <stefan.schmutz@outlook.com>
2020-10-27 15:25:15 +0100

    remove/replace parts remaining from previous app

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-# SARS2MOLIS
+# Utility2MOLIS
 
 library(shiny)
 library(stringr)
@@ -11,12 +11,16 @@ library(tidyverse)
 shinyServer(function(input, output, session) {
     
     raw_data <- reactive({
+
+        # only run if a pcr_file is uploaded
+        req(input$pcr_file)
+      
         read_excel(input$pcr_file$datapath) %>%
             select(`File name`, `Creation date/time`, `Negative control ID`, `Sample ID`, `Channel No.`,
                    `Sample overall result (orig.)`, `Target result (orig.)`, `RFI`, `CT (orig.)`, `Channel result (orig.)`)
     })
     
-    file <- reactive({raw_data()$`File name`[1]})
+    filename <- reactive({raw_data()$`File name`[1]})
     date <- reactive({raw_data()$`Creation date/time`[1]})
     NC <- reactive({raw_data()$`Negative control ID`[1]})
    
@@ -24,12 +28,15 @@ shinyServer(function(input, output, session) {
         raw_data() %>%
             select(-`File name`, -`Creation date/time`, -`Negative control ID`) %>%
             mutate(Ct = as.numeric(`CT (orig.)`)) %>%
-            mutate(Target = ifelse(`Channel No.` == 2, "E_Gen", "IC")) %>%
+            mutate(Target = case_when(
+                `Channel No.` == 2 ~ "E_Gen",
+                `Channel No.` == 5 ~ "IC",
+                TRUE ~ NA_character_)) %>%
             select(`Sample ID`, Target, Ct) %>%
-            spread(key = Target, value = Ct) %>%
+            pivot_wider(names_from = Target, values_from = Ct) %>%
             mutate(Result = case_when(
                 E_Gen < 45 ~ "Positiv",
-                IC >= 25 & IC <= 40 & is.na(E_Gen) ~ "Negativ",
+                IC >= 30 & IC <= 40 & is.na(E_Gen) ~ "Negativ",
                 TRUE ~ "Invalid")) %>%
             mutate(VL = signif(10^((E_Gen - 47.251)/-3.272),2)) %>%  #-3.272	47.251
             arrange(Result, `Sample ID`) %>%
@@ -41,50 +48,26 @@ shinyServer(function(input, output, session) {
         tidy_data() 
     })
 
-    ### Export
-    molis_out <- reactive({
-        table() %>% select(c("sample_name", "SARS_ct", "GAPDH_ct", "MS2_ct", "result", "flag"))
-    })
     
    
     ### Download
-    output$molis_export <- downloadHandler(
-        
-        filename = function() {
-            paste0("molis-", Sys.Date(), ".txt")
-        },
-        
-        content = function(file) {
-            write.table(molis_out(),
-                        file,
-                        quote = FALSE,
-                        sep ='\t',
-                        row.names = FALSE,
-                        eol = "\r\n",
-                        append = FALSE)
-        })
-    
     output$pdf_export <- downloadHandler(
         
         filename = function() {
-            from_molis <- if_else(molis_min() < 1000000000, true = molis_min()+1000000000, false = molis_min())
-            to_molis <- if_else(molis_max() < 1000000000, true = molis_max()+1000000000, false = molis_max())
-            
-            paste0(from_molis, "_to_", to_molis, ".pdf")
+            # take the same base name of the export as the report file name
+            base_name <- sub("\\.xls.*$", "", input$pcr_file$name) 
+            paste0(base_name, ".pdf")
         },
         
         content = function(file) {
             tempReport <- file.path(tempdir(), "report.Rmd")
             file.copy("report.Rmd", tempReport, overwrite = TRUE)
             
-            params <- list(filename = input$pcr_file$name,
-                           end_time = end_time_out(),
-                           cycler_nr = cycler_nr_out(),
-                           plot = plot_out(),
-                           raw_data = raw_data(),
-                           molis_out_table = molis_out(),
-                           MS2_median = round(median(table()$MS2_ct_dbl, na.rm = TRUE), digits = 1),
-                           MS2_sd = round(sd(table()$MS2_ct_dbl, na.rm = TRUE), digits = 3))
+            params <- list(filename = filename(),
+                           date = date(),
+                           tidy_data = tidy_data(),
+                           git_tag = system("git describe --abbrev=0 --tags", intern=TRUE),
+                           git_id_short = system("git rev-parse --short HEAD", intern=TRUE))
             
             rmarkdown::render(tempReport, output_file = file,
                               params = params,
